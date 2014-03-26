bd73f2743c84c482925ec79eb2653743cbc4745e
Anthony Domenico <mryesofyes@gmail.com>
2014-03-26 16:28:19 -0400

    Further cleaning up

--- src/tetrisgame/BlockGrid.java
+++ src/tetrisgame/BlockGrid.java
@@ -11,124 +11,120 @@ public class BlockGrid
     private int height;
     private int drawX;
     private int drawY;
-    
+
     //Array of inactive tetris blocks
     private ArrayList<Block> blocks;
 
     public BlockGrid(int width, int height, int drawX, int drawY)
     {
-        this.width  = width;
+        this.width = width;
         this.height = height;
-        this.drawX  = drawX;
-        this.drawY  = drawY;
+        this.drawX = drawX;
+        this.drawY = drawY;
         blocks = new ArrayList<Block>();
     }
-    
-    //Return height of blockGrid
+
     public int getHeight()
     {
         return height;
     }
-    
-    //Return width of blockGrid
+
     public int getWidth()
     {
         return width;
     }
-    
-    //Get current x on blockGrid
+
     public int getX()
     {
         return drawX;
     }
-    
-    //Get current y on blockGrid
+
     public int getY()
     {
         return drawY;
     }
-    
-    //Determine is space is active
+
+    // Determine is space is active
     public boolean isBlockHere(int x, int y)
-    {	
-    	//If block does not lie in current position
+    {
+        // If block does not lie within bounds
         if ((x >= width) || (y >= height))
             return false;
-        
-        //Loop through Block grid and determine if block lies in position
+
+        // Loop through Block grid and determine if block lies in position
         for (Block block : blocks)
             if (block.getX() == x && block.getY() == y)
                 return true;
 
         return false;
     }
-    
+
     //Place inactive Tetris piece inside the blockGrid
     //Parameters: x = curr x, y = curr y, color = current tetris color
     public void placeBlock(int x, int y, Color color)
     {
         if ((x >= width) || (y >= height))
             return;
-        blocks.add(new Block(x, y, color));		//Add block to array
+        blocks.add(new Block(x, y, color));
     }
-    
+
     //Draw blockGrid
     public void draw(Graphics2D g2)
     {
         g2.setColor(Color.BLACK);
-        g2.fillRect(drawX, drawY, width*TetrisPiece.BLOCK_WIDTH, height*TetrisPiece.BLOCK_HEIGHT);
+        g2.fillRect(drawX, drawY, width * TetrisPiece.BLOCK_WIDTH, height * TetrisPiece.BLOCK_HEIGHT);
 
         for (Block block : blocks) {
             g2.setColor(block.getColor());
-            g2.fill(new Rectangle2D.Double(drawX + block.getX()*TetrisPiece.BLOCK_WIDTH,
-                                           drawY + block.getY()*TetrisPiece.BLOCK_HEIGHT,
+            g2.fill(new Rectangle2D.Double(drawX + block.getX() * TetrisPiece.BLOCK_WIDTH,
+                                           drawY + block.getY() * TetrisPiece.BLOCK_HEIGHT,
                                            TetrisPiece.BLOCK_WIDTH,
                                            TetrisPiece.BLOCK_HEIGHT));
         }
     }
-    
+
     //Remove a full row
     public void removeRow(int rowToRemove)
     {
         // Clear row
-        Iterator<Block> it = blocks.iterator();		//Iterator pattern to remove all blocks in full row
-        while (it.hasNext()) {	
-            Block block = it.next();	//sets next block in array to next
+        Iterator<Block> it = blocks.iterator();
+        while (it.hasNext()) {
+            Block block = it.next();
             if (block.getY() == rowToRemove)
-                it.remove();		//removes full Y row
+                it.remove(); // removes full Y row
         }
 
         // Drop rows
-        for (Block block : blocks) {	//For rows not full
+        for (Block block : blocks) {
             if (block.getY() < rowToRemove)
-                block.dropOnce();	//drop down (0,1)
+                block.dropOnce();  // drop down (0,1)
         }
     }
-    
+
     //Removes full rows
     public void removeFullRows()
     {
-        for (int row = 0; row < getHeight(); row++)	//Removes multiple full rows
+        for (int row = 0; row < getHeight(); row++) // Removes multiple full rows
             if (rowIsFull(row))
                 removeRow(row);
     }
-    
+
     //Boolean determines whether row is full 
     public boolean rowIsFull(int row)
     {
-        return numberOfBlocksInRow(row) == width;	//Compares number of blocks in row to the width (each block is 1 space)
-    }	
-    
+        return numberOfBlocksInRow(row) == width; // Note: each block is 1 space
+    }
+
     //Returns an integer with how many blocks are in the row
     private int numberOfBlocksInRow(int row)
     {
-        int count = 0;	//sets original count to 0
+        int count = 0;
         for (Block block : blocks)
-            if (block.getY() == row)		//if block is in a Y
-                count++;		//Add to count
+            if (block.getY() == row)
+                count++;
         return count;
     }
-    
+
     private class Block
     {
         private int x, y;
@@ -141,10 +137,24 @@ public class BlockGrid
             this.color = color;
         }
 
-        public int getX() { return x; }
-        public int getY() { return y; }
-        public Color getColor() { return color; }
+        public int getX()
+        {
+            return x;
+        }
 
-        public void dropOnce() { this.y++; }	//Drop once by adding 1 row to the y
+        public int getY()
+        {
+            return y;
+        }
+
+        public Color getColor()
+        {
+            return color;
+        }
+
+        public void dropOnce()
+        {
+            this.y++;
+        }    //Drop once by adding 1 row to the y
     }
 }
--- src/tetrisgame/TetrisGame.java
+++ src/tetrisgame/TetrisGame.java
@@ -6,46 +6,45 @@ import javax.swing.*;
 
 public class TetrisGame extends JFrame
 {
-    public static final int GAME_WIDTH  = 350,
-                            GAME_HEIGHT = 400;
-    
-    JPanel tetrisPanel = new TetrisPanel(this);	//Create instance of tetrisPanel
-    JPanel gamePanel;		//Instantiate upon pressing start
-    
+    public static final int GAME_WIDTH = 350,
+            GAME_HEIGHT = 400;
+
+    JPanel tetrisPanel = new TetrisPanel(this);
+    JPanel gamePanel; // Instantiate upon pressing start
+
     public TetrisGame()
     {
         super("Tetris");
 
         this.setBackground(java.awt.Color.white);
         this.setPreferredSize(new java.awt.Dimension(GAME_WIDTH, GAME_HEIGHT));
-        this.setSize(new java.awt.Dimension(GAME_WIDTH, GAME_HEIGHT));      
-        //Add tetrisPanel to frame
+        this.setSize(new java.awt.Dimension(GAME_WIDTH, GAME_HEIGHT));
+
         this.add(tetrisPanel);
- 
+
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
-        
+
     }
-    
+
     /*
      * called inside of TetrisPanel
      * 
      * Adds GamePanel to the TetrisPanel which replaces start button and 
      * instruction JLabel
      */
-    public void addGamePanel(){
-    	
-    	//Remove tetrisPanel
-    	this.remove(tetrisPanel);	
-    	this.revalidate();	
-    	//Add a new instance of gamePanel
-    	this.add(gamePanel = new GamePanel());	//Timer starts upon start
-    	gamePanel.requestFocus();		//Sets focus back to gamePanel 	
-    	this.revalidate();			//Re-validate
-    	repaint();				//Repaint
+    public void addGamePanel()
+    {
+        this.remove(tetrisPanel);
+        this.revalidate();
+
+        this.add(gamePanel = new GamePanel()); // Timer starts upon start
+        gamePanel.requestFocus(); // Sets focus back to gamePanel
+        this.revalidate();
+        repaint();
     }
-    
+
     public static void main(String[] args)
     {
         new TetrisGame();
